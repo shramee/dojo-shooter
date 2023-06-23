494cb616d00e1a1a9d6660a4b850b27e0d99e24a
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-06-24 01:35:56 +0800

    Zombies dummy render

--- client/static/dojo.js
+++ client/static/dojo.js
@@ -1,4 +1,5 @@
 class DojoCalls {
+  zombies_on_chain = [];
   constructor() {
     this.world_addr = ecs_data.world_addr;
     // this.rpc = new starknet.RpcProvider({
@@ -30,8 +31,52 @@ class DojoCalls {
     });
     return await req.json();
   }
+  async fetch_zombies() {
+    let response = await window.dojo.raw_fetch('starknet_call', [
+      {
+        contract_address:
+          '0x7f1d6c1b15e03673062d8356dc1174d5d85c310479ec49fe781e8bf89e4c4f8',
+        entry_point_selector:
+          '0x027706e83545bc0fb130476239ec381f65fba8f92e2379d60a07869aa04b4ccc',
+        calldata: ['0x5a6f6d626965', '0'],
+      },
+      'pending',
+    ]);
+
+    let { result } = response;
+
+    if (!result) {
+      return console.log('Error occurred', response);
+    }
+
+    let zombies_count = parseInt(result[0]);
+    let starting_index = 2 + zombies_count;
+    let zombie_data_gap = 1 + parseInt(result[starting_index]); // Gap is
+
+    this.zombies_on_chain = [];
+
+    for (let i = 0; i < zombies_count; i++) {
+      // zi is index of individual Zombie data span
+      let zi = starting_index + i * zombie_data_gap;
+      //   console.log(i, zi, result[zi]);
+      let x = parseInt(result[zi + 1]);
+      let y = parseInt(result[zi + 2]);
+
+      if (x >= 4294967296) {
+        x -= 4294967296;
+        x *= -1;
+      }
+      if (y >= 4294967296) {
+        y -= 4294967296;
+        y *= -1;
+      }
+
+      this.zombies_on_chain.push([x, y]);
+    }
+    // this.zombies_on_chain = result;
+  }
 }
 
 window.dojo = new DojoCalls();
-
-setInterval(async () => {}, 100);
+window.dojo.fetch_zombies();
+// setInterval(() => window.dojo.fetch_zombies(), 10000);
--- client/static/sketch.js
+++ client/static/sketch.js
@@ -1,7 +1,29 @@
+let vmin = 500;
+
 function setup() {
-  createCanvas(440, 440);
+  vmin = Math.min(windowHeight, windowWidth);
+  createCanvas(vmin, vmin);
+  noStroke();
+}
+
+function coord(val, is_y = false) {
+  if (is_y) val *= -1;
+  let display_field = vmin / 2;
+  return (val * display_field) / 1000;
 }
 
 function draw() {
-  background(220);
+  translate(vmin / 2, vmin / 2);
+  background(240, 240, 240);
+  fill(255, 0, 0);
+  ellipse(0, 0, 25, 25);
+
+  fill(0, 0, 0);
+
+  window.dojo.zombies_on_chain.forEach(([x, y]) => {
+    console.log(coord(x), coord(y, 'y'));
+    ellipse(coord(x), coord(y, 'y'), 5, 5);
+    text(`${x}, ${y}`, coord(x), coord(y, 'y') - 5);
+    noLoop();
+  });
 }
