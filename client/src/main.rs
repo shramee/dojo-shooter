5148ff2226998caedb23500465305713f73c40d9
sugarglider7 <sugar_glider7@outlook.com>
2023-06-24 12:34:00 +0200

    Update main.rs

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,12 +1,38 @@
 extern crate rocket;
 extern crate rocket_contrib;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
 
 use std::{env, fs};
 
 use rocket_contrib::serve::StaticFiles;
 
+pub struct CORS;
+
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    fn on_response(&self, request: &Request, response: &mut Response) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", StaticFiles::from("static"))
+    rocket::ignite()
+        .mount("/", StaticFiles::from("static"))
+        .attach(CORS)
 }
 
 fn main() {
